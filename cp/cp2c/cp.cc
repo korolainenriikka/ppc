416846f85eed79b61447070cfafb75dc3b6882a5
Riikka Korolainen <riikka.korolainen@gmail.com>
2024-02-05 13:51:38 +0200

    pass

--- cp/cp2c/cp.cc
+++ cp/cp2c/cp.cc
@@ -4,6 +4,10 @@
 // baseline speed (local)
 // benchmarks/2.txt     3.792s  pass
 
+// test 1 correct normalized
+//-0.707107 0.707107 
+//-0.707107 0.707107
+
 typedef double double4_t __attribute__ ((vector_size (4 * sizeof(double))));
 constexpr float infty = std::numeric_limits<float>::infinity();
 
@@ -20,8 +24,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
 
     // CREATE NORMALIZED MATRIX
     // create vector size ny*nx
-    // vectorized: nx should be divisible by 4
-    int row_length = nx + (4-(nx%4));
+    int ncols = nx + (4-nx%4);
     std::vector<double> normalized(ny*nx, 0.0);
 
     for (int y = 0; y < ny; ++y) {
@@ -33,67 +36,66 @@ void correlate(int ny, int nx, const float *data, float *result) {
         //std::cout << mean << " ";
     
         // vähennä mean jokaisesta elementistä
-        for (int x = 0; x < nx; ++x) {
+        for (int x = 0; x < nx; ++x){
+            int i = x + y*nx;
             if (mean != 0) {
-                normalized[x + y*nx] = data[x + y*nx] - mean;
+                normalized[i] = data[i] - mean;
             } else {
-                normalized[x + y*nx] = data[x + y*nx];
+                normalized[i] = data[i];
             }
         }
+        //std::cout << "\n";
     }
 
-    // elements per vector
-    constexpr int nb = 4;
-    // vectors per row
-    int na = nx / nb;
-    
     // normalize the input rows so that for each row the sum of the squares of the elements is 1
     for (int y = 0; y < ny; ++y) {
         double sum_of_squares = 0.0;
         for (int x = 0; x < nx; ++x) {
-            sum_of_squares += normalized[x + y*row_length]*normalized[x + y*row_length];
+            int i = x + y*nx;
+            sum_of_squares += normalized[i]*normalized[i];
         }
         // jaa sum of squares neliöjuurella jokainen
         double sqrt_sum_of_squares = sqrt(sum_of_squares);
-        std::cout << sqrt_sum_of_squares;
 
-        for (int x = 0; x < na; ++x) {
-            //for (int z = 0; z < nb; ++z) {
-            //int index = z + z * x + y*nx;
-            int index = x + y*nx;
-            normalized[index] = index >= nx ? infty : normalized[index] / sqrt_sum_of_squares;
-            //}
+        for (int x = 0; x < nx; ++x) {
+            int i = x + y*nx;
+            normalized[i] = normalized[i] / sqrt_sum_of_squares;
         }
     }
     
     // MATRIX PRODUCT
+    // diagonal elements are always 1
     for (int a = 0; a < ny; ++a) {
         result[a + a*ny] = 1;
     }
 
-
-    // VECTORIZATION
-    // add padding to rows: x-dimension should be divisible by 4
-
     // print normalized
-    for (int y = 0; y < ny; ++y) {
-        for (int x = 0; x < row_length; ++x) {
-            std::cout << normalized[x + y*nx] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-
-    // accumulate 4 sums of multiplications --> sum together before result = dot product
-    
     // for (int y = 0; y < ny; ++y) {
-    //     for (int x = y+1; x < ny; ++x) {
-    //         double dot_product = 0.0;
-    //         double4_t dot_product_v = 0.0
-    //         for (int k = 0; k < nx; ++k) {
-    //             dot_product += normalized[k + y*nx] * normalized[k + x*nx];
-    //         }
-    //         //result[x + y * ny] = dot_product;
+    //     for (int x = 0; x < nx; ++x) {
+    //         std::cout << normalized[x + y*nx] << " ";
     //     }
+    //     std::cout << "\n";
     // }
+    // std::cout << "\n";
+
+    // Calculate the (upper triangle of the) matrix product Y = XX^T
+    for (int y = 0; y < ny; ++y) {
+        for (int x = y+1; x < ny; ++x) {
+            //std::cout << y;
+            //std::cout << x;
+            //yth row and xth row
+            double dot_product = 0.0;
+            for (int k = 0; k < nx; ++k) {
+                // rows i and j in index k
+                //std::cout << normalized[k + y*nx] << " " << normalized[k + x*nx] << "\n";
+                dot_product += normalized[k + y*nx] * normalized[k + x*nx];
+            }
+            //std::cout <<dot_product;
+            result[x + y * ny] = dot_product;
+        }
+    }
+
+    // independent operations
+    // multiplications of components of the dot product
+    // computations of individual dot products
 }
