e848f931bec051d3af5ed7021e3692b52fdf8149
Riikka Korolainen <riikka.korolainen@gmail.com>
2024-03-11 15:42:48 +0200

    Two-thread quicksort

--- sorting/so5/so.cc
+++ sorting/so5/so.cc
@@ -1,9 +1,37 @@
 #include <algorithm>
+#include <iostream>
 
 typedef unsigned long long data_t;
 
 void psort(int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of quicksort.
-    std::sort(data, data + n);
+    if (n==0) {
+        return;
+    }
+    int r = n-1;
+    int p = 0;
+    int q = p;
+    int j = p;
+
+    data_t pivot = data[r];
+    // std::cout << pivot << '\n';
+    while (j < r) {
+        if (data[j] < pivot) {
+            data_t swp = data[j];
+            data[j] = data[q];
+            data[q] = swp;
+            q += 1;
+        }
+        j += 1;
+    }
+
+    data[r] = data[q];
+    data[q] = pivot;
+
+    #pragma omp sections
+    {
+        #pragma omp section
+        std::sort(data, data + q);
+        #pragma omp section
+        std::sort(data + q, data + n);
+    }
 }
