38ffa254d12a594ec692bda6f223d96acd705841
Riikka Korolainen <riikka.korolainen@gmail.com>
2024-03-11 13:24:20 +0200

    fast omp cp3a

--- cp/cp3a/cp.cc
+++ cp/cp3a/cp.cc
@@ -3,6 +3,15 @@
 #include <iostream>
 typedef double double4_t __attribute__ ((vector_size (4 * sizeof(double))));
 
+constexpr int nb = 4;
+double vec_sum(double4_t vec) {
+    double sum = 0.0;
+    for (int i = 0; i<nb; ++i) {
+        sum += vec[i];
+    }
+    return sum;
+}
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -13,7 +22,6 @@ This is the function you need to implement. Quick reference:
 */
 void correlate(int ny, int nx, const float *data, float *result) {
     // number of columns in normalized matrix with padding
-    constexpr int nb = 4;
     // vectors per row
     int na = nx % nb == 0 ? nx / nb : (nx/nb)+1;
     bool has_padding = nx % nb != 0;
@@ -26,6 +34,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
     std::vector<double4_t> vnorm(ny*na);
 
     // kopioi datan sisältö vektorimuodon dataan
+    #pragma omp parallel for schedule(static, 1)
     for (int y = 0; y < ny; ++y) {
         for (int a = 0; a < na; ++a) {
             for (int b = 0; b < nb; ++b) {
@@ -36,6 +45,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
         }
     }
 
+    #pragma omp parallel for schedule(static, 1)
     for (int y = 0; y < ny; ++y) {
         double4_t vsum = {0.0, 0.0, 0.0, 0.0};
         for (int a = 0; a < na; ++a) {
@@ -55,6 +65,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
     int no_of_padding = nb - (nx % nb);
     // vikan vektorin indeksi on na-1
     if (has_padding){
+        #pragma omp parallel for schedule(static, 1)
         for (int y = 0; y < ny; ++y) {
             for(int b = nb-no_of_padding; b < nb; ++b) {
                 vnorm[(na-1) + na*y][b] = 0.0;
@@ -63,6 +74,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
     }
 
     // normalize the input rows so that for each row the sum of the squares of the elements is 1
+    #pragma omp parallel for schedule(static, 1)
     for (int y = 0; y < ny; ++y) {
         // sos = sum of squares
         double4_t vsos =  {0.0, 0.0, 0.0, 0.0};
@@ -99,28 +111,91 @@ void correlate(int ny, int nx, const float *data, float *result) {
     // TÄSTÄ ALKAEN NELIÖ KERRALLAAN MUUTOKSET
     //
 
+    constexpr double4_t zeros = {
+        0.0, 0.0, 0.0, 0.0
+    };
+
     // Calculate the (upper triangle of the) matrix product Y = XX^T
-    //#pragma omp parallel for schedule(static, 1)
     int count_of_squares_y = std::floor((ny / 3) - 1);
     int count_of_squares_x = std::floor(ny / 3);
 
+    #pragma omp parallel for schedule(static, 1)
     for (int y = 0; y < count_of_squares_y * 3; y += 3) {
         for (int x = (std::floor(y/3) + 1) * 3; x < count_of_squares_x * 3; x += 3) {
-            // nine sums
-            // vector<double> dot_prods =
+            // nine sums first index: y second index: x
+            double4_t dp_vecs[3][3];
+            for (int id = 0; id < 3; ++id) {
+                for (int jd = 0; jd < 3; ++jd) {
+                    dp_vecs[id][jd] = zeros;
+                }
+            }
 
-            //yth row and xth row
-            // akkumuloidaan neljää summaa
-            double4_t dot_product_v = {0.0, 0.0, 0.0, 0.0};
             for (int k = 0; k < na; ++k) {
-                // rows i and j in index k
-                // luo vektori mis on neljä tuloo
-                // rivi x ja y
+                double4_t y0 = vnorm[na*(y + 0) + k];
+                double4_t y1 = vnorm[na*(y + 1) + k];
+                double4_t y2 = vnorm[na*(y + 2) + k];
+                double4_t x0 = vnorm[na*(x + 0) + k];
+                double4_t x1 = vnorm[na*(x + 1) + k];
+                double4_t x2 = vnorm[na*(x + 2) + k];
+                dp_vecs[0][0] += y0 * x0;
+                dp_vecs[0][1] += y0 * x1;
+                dp_vecs[0][2] += y0 * x2;
+                dp_vecs[1][0] += y1 * x0;
+                dp_vecs[1][1] += y1 * x1;
+                dp_vecs[1][2] += y1 * x2;
+                dp_vecs[2][0] += y2 * x0;
+                dp_vecs[2][1] += y2 * x1;
+                dp_vecs[2][2] += y2 * x2;
+            }
+            for (int yd = 0; yd < 3; ++yd) {
+                for (int xd = 0; xd < 3; ++xd) {
+                    // me ollaan resultissa niin että ylävasen on rivi y sarake x
+                    // se on resultissa kohta y*ny + x
+                    // mutta resultin sisällä ollaan rivillä yd ja sarakkeella xd
+                    // isossa matriisissa se on rivi y+yd sarake x+xd
+                    // eli se on resultsissa kohta (y+yd)*ny +(x+xd)
+                    result[(y+yd)*ny +(x+xd)] = vec_sum(dp_vecs[yd][xd]);
+                }
+            }
+        }
+    }
+
+    // OIKEALLE JÄÄVÄT TEKEMÄTTÖMÄT 0 1 tai 2 SARAKETTA
+    int no_of_leftover_cols = ny%3;
+    for (int y = 0; y < ny; ++y) {
+        for (int x_leftover = 0; x_leftover < no_of_leftover_cols; ++x_leftover) {
+            int x = count_of_squares_x*3 + x_leftover;
+
+            double4_t dot_product_v = zeros;
+            for (int k = 0; k < na; ++k) {
                 double4_t tulot = vnorm[k + y*na] * vnorm[k + x*na];
                 dot_product_v += tulot;
             }
-            double dot_product = dot_product_v[0]+dot_product_v[1]+dot_product_v[2]+dot_product_v[3];
+            double dot_product = vec_sum(dot_product_v);
             result[x + y * ny] = dot_product;
         }
+    }
+
+    // KESKELLE JÄÄVÄT TEKEMÄTTÖMÄT PIKKUKOLMIOT
+    #pragma omp parallel for schedule(static, 1)
+    for (int y = 0; y < (count_of_squares_y + 1) * 3; y += 3) {
+        int x = y;
+        double4_t dp_vecs[3];
+        for (int id = 0; id < 3; ++id) {
+            dp_vecs[id] = zeros;
+        }
+
+        for (int k = 0; k < na; ++k) {
+            double4_t y0 = vnorm[na*(y + 0) + k];
+            double4_t y1 = vnorm[na*(y + 1) + k];
+            double4_t x1 = vnorm[na*(x + 1) + k];
+            double4_t x2 = vnorm[na*(x + 2) + k];
+            dp_vecs[0] += y0 * x1;
+            dp_vecs[1] += y0 * x2;
+            dp_vecs[2] += y1 * x2;
+        }
+        result[(y+0)*ny +(x+1)] = vec_sum(dp_vecs[0]); // yd = 0 xd = 1
+        result[(y+0)*ny +(x+2)] = vec_sum(dp_vecs[1]); // yd = 0 xd = 2
+        result[(y+1)*ny +(x+2)] = vec_sum(dp_vecs[2]); // yd = 1 xd = 2
     }
 }
