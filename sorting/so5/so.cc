89ee480ac482352460d9a6fc92de602d387071f3
Riikka Korolainen <riikka.korolainen@gmail.com>
2024-03-11 17:52:05 +0200

    Until quicksort

--- sorting/so5/so.cc
+++ sorting/so5/so.cc
@@ -1,19 +1,15 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
+#include <omp.h>
 
 typedef unsigned long long data_t;
 
-void psort(int n, data_t *data) {
-    if (n==0) {
-        return;
-    }
-    int r = n-1;
-    int p = 0;
+int pivot(int p, int r, data_t* data) {
     int q = p;
     int j = p;
 
     data_t pivot = data[r];
-    // std::cout << pivot << '\n';
     while (j < r) {
         if (data[j] < pivot) {
             data_t swp = data[j];
@@ -26,12 +22,48 @@ void psort(int n, data_t *data) {
 
     data[r] = data[q];
     data[q] = pivot;
+    return q;
+}
 
-    #pragma omp sections
-    {
-        #pragma omp section
-        std::sort(data, data + q);
-        #pragma omp section
-        std::sort(data + q, data + n);
+void psort(int n, data_t* data) {
+    if (n==0) {
+        return;
+    }
+    int num_threads = omp_get_max_threads();
+    int len_qs = num_threads + 1;
+    std::vector<int> qs(len_qs,n);
+    qs[0] = 0;
+    qs[1] = n;
+
+    // zero and n always included
+    int num_qs = 2;
+    while (num_qs < len_qs-1) {
+        // find largest section start and end
+        int start = 0;
+        int end = 0; // end exclusive
+        for (int i = 0; i < len_qs-1; ++i) {
+            if (qs[i] == n) {
+                break;
+            }
+            int sec_start = qs[i];
+            int sec_end = qs[i+1];
+            if (sec_end-sec_start > end - start) {
+                start = sec_start;
+                end = sec_end;
+            }
+        }
+        // pivot: remove last element
+        int new_pivot = pivot(start, end-1, data);
+        qs[num_qs-1] = new_pivot;
+        std::sort(qs.begin(), qs.begin()+len_qs);
+        ++num_qs;
+    }
+    for (int i = 0; i < len_qs; ++i) {
+        std::cout << qs[i] << ' ';
+    }
+
+    #pragma omp parallel for schedule(static, 1)
+    for (int i = 1; i <= num_qs; ++i){
+        std::sort(data + qs[i-1], data + qs[i]);
     }
 }
